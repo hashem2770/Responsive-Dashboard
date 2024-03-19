49efe41711e2078f5401460b589aa262b062f83c
hashem <hashemhassen0@gmail.com>
2024-03-19 21:05:19 +0200

    practice layout Builder

--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,86 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.sizeOf(context).height;
-    log(height.round().toString());
+    double width = MediaQuery.sizeOf(context).width;
+
     return Scaffold(
-      body: Column(
-        children: [
-          Container(color: Colors.amberAccent,height: height*.25,),
-          Container(color: Colors.blue,height: height*.25,),
-          Container(color: Colors.red,height: height*.25,),
-        ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          log(constraints.maxWidth.toString()); //minWidth:450
+          return constraints.maxWidth < 800
+              ? const MobileLayout()
+              : const DesktopLayout();
+        },
       ),
+    );
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+              margin: const EdgeInsets.all(8),
+              color: Colors.cyan,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  index.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 30),
+                ),
+              ));
+        });
+  }
+}
+
+class DesktopLayout extends StatelessWidget {
+  const DesktopLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                    margin: const EdgeInsets.all(8),
+                    color: Colors.cyan,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        index.toString(),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ));
+              }),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.amber,
+            child: const Text('Desktop Layout'),
+          )
+        )
+      ],
     );
   }
 }
